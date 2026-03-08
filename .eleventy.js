c7fe71126f1766a6cb64c516e6f819ec26a1950a
mrm1987 <mozola.roma@gmail.com>
2026-03-08 19:06:43 +0100

    Final fix for /agd/ structure and clean URLs

--- .eleventy.js
+++ .eleventy.js
@@ -5,16 +5,15 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/fonts");
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
-    eleventyConfig.addPassthroughCopy("src/_redirects");
-    eleventyConfig.addPassthroughCopy("src/robots.txt");
-    eleventyConfig.addPassthroughCopy("src/sitemap.xml");
-    eleventyConfig.addPassthroughCopy("src/policy.html");
+    eleventyConfig.addPassthroughCopy({ "src/robots.txt": "robots.txt" });
+    eleventyConfig.addPassthroughCopy({ "src/sitemap.xml": "sitemap.xml" });
+    eleventyConfig.addPassthroughCopy({ "src/policy.html": "agd/policy.html" });
 
     return {
         pathPrefix: "/agd/",
         dir: {
             input: "src",
-            output: "_site/agd",
+            output: "_site",
             includes: "_includes",
             layouts: "_layouts"
         },
