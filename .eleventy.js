91380d367948cb7dd45d2989921ab7edbbb825b0
mrm1987 <mozola.roma@gmail.com>
2026-03-08 14:56:12 +0100

    Add Cloudflare _redirects: /agd/* rewrite to fix CSS paths

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/fonts");
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
+    eleventyConfig.addPassthroughCopy("src/_redirects");
 
     return {
         pathPrefix: "/agd/",
