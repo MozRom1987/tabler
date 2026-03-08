73b32afb0b9a2b3a035f3d882a042528ae6d154a
mrm1987 <mozola.roma@gmail.com>
2026-03-08 18:38:10 +0100

    Cleaned up root directory, moved technical files to src, and updated sitemap/robots

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,9 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/fonts");
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
     eleventyConfig.addPassthroughCopy("src/_redirects");
+    eleventyConfig.addPassthroughCopy("src/robots.txt");
+    eleventyConfig.addPassthroughCopy("src/sitemap.xml");
+    eleventyConfig.addPassthroughCopy("src/policy.html");
 
     return {
         pathPrefix: "/agd/",
